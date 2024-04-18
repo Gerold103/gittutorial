2c36ab7bc22e52731926e2767a2c08a405e0fc1d
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2024-04-18 13:28:35 +0200

    [wip] Flibber in foo

--- moduleFoo.cpp
+++ moduleFoo.cpp
@@ -6,6 +6,12 @@ ObjectFoo::zorble()
 	myZorble = 'a' + (myZorble - 'a' + 1) % ('z' - 'a' + 1);
 }
 
+void
+ObjectFoo::flibber()
+{
+	myFlibbers.push_back(makeFlibber());
+}
+
 ObjectFoo
 makeFoo(int wibble)
 {
