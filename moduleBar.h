7f96205b8b60824ff3269b3d96c94b7e0343992a
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2024-04-18 13:28:28 +0200

    [wip] Flibber in bar

--- moduleBar.h
+++ moduleBar.h
@@ -1,14 +1,18 @@
 #pragma once
 
-#include <string>
+#include "core.h"
+
+#include <vector>
 
 struct ObjectBar
 {
 	void splinx();
 	void yibble() { myYibble = myYibble + 1; }
+	void flibber();
 
 	std::string mySplinx = "Emptio";
 	uint64_t myYibble = 0;
+	std::vector<Flibber> myFlibbers;
 };
 
 ObjectBar makeBar();
